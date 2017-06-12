f72fe32ef1ef70a7232504dca6eabd21ee4ce424
Julio Mixco <jmixco@applaudostudios.com>
2017-06-12 11:24:54 -0600

    track event

--- public/script.js
+++ public/script.js
@@ -1,43 +1,49 @@
 angular.module('app', []);
-angular.module('app').controller('searchController', ['$scope', '$http','$window', function ($scope, $http, $window) {
-    var that = $scope;
-    that.apiURL = 'http://localhost:3000/api';
-    that.products = [];
-    that.filter = '';
-    that.doFilter = false;
-    that.init = (host) => {
-        if (host) {
-            that.apiURL = host;
-        }
-        that.updateProducts();
+angular.module('app').controller('searchController', ['$scope', '$http', '$window', function ($scope, $http, $window) {
+  var that = $scope;
+  that.apiURL = 'http://localhost:3000/api';
+  that.products = [];
+  that.filter = '';
+  that.doFilter = false;
+  that.init = (host) => {
+    if (host) {
+      that.apiURL = host;
+    }
+    that.updateProducts();
 
-    };
-    that.onFilter = () => {
-        if (!that.filter || that.filter === '') {
-            that.doFilter = false;
-           
-        } else {
-            that.doFilter = true;            
-        }
-        console.log("filter", that.filter);
-        that.updateProducts();
-    };
-    that.updateProducts = () => {
-      $window.ga.trackEvent('api request', "update products","23123");
-        that.getProducts()
-            .then(data => {
-                //console.log(data);
-                that.products = data.data;
-            }, error => {
-                console.log(error);
-            });
-    };
-    that.getProducts = () => {
-        var url = that.apiURL+'/product';
-        if (that.doFilter) {
-            url += '?name=' + that.filter;
-        }
-        console.log('request: ', url);
-        return $http.get(url);
-    };
+  };
+  that.onFilter = () => {
+    if (!that.filter || that.filter === '') {
+      that.doFilter = false;
+
+    } else {
+      that.doFilter = true;
+    }
+    console.log("filter", that.filter);
+    that.updateProducts();
+  };
+  that.updateProducts = () => {
+    $window.ga.trackEvent('api request', "update products", "23123");
+    $window.ga('send', {
+      hitType: 'event',
+      eventCategory: 'api request',
+      eventAction: 'update products',
+      eventLabel: '23123'
+    });
+    that.getProducts()
+      .then(data => {
+        //console.log(data);
+        that.products = data.data;
+      }, error => {
+        console.log(error);
+      });
+  };
+  that.getProducts = () => {
+    var url = that.apiURL + '/product';
+    if (that.doFilter) {
+      url += '?name=' + that.filter;
+    }
+    console.log('request: ', url);
+    return $http.get(url);
+  };
 }]);
